b1d37b16fcac2b6ebad23b675ee5d4c73cb9a4a5
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-10 19:36:11 +0530

    Routes to main pages

---
+++ src/Components/Routes.jsx
@@ -0,0 +1,36 @@
+import Shop from './Shop.jsx';
+import Cart from './Cart.jsx';
+import Errorpage from './Errorpage.jsx';
+import Home from './Home.jsx';
+import About from './About.jsx';
+
+const routes = [
+
+  {
+    path:"/",
+    element: <Home/>,
+    errorElement: <Errorpage/>,
+    children : [
+
+        {
+            path:"Shop",
+            element:<Shop/>
+        },
+        {
+            path: "Cart", 
+            element:<Cart/>
+        }, 
+        {
+            path: "About",
+            element:<About/>
+        }
+
+    ]
+  },
+  {
+    path:"Home",
+    element:<Home/>
+  },
+];
+
+export default routes;
