87c53f3b50f8a7b1401ed8d28fcfed4894f562cb
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-10 21:45:21 +0530

    temp commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,16 @@
+import huntingImg from "../Assets/Hunting.jpeg";
+
 function About() {
     return (
-      <div>Welcome to the ABOUT page</div>
+      <>
+
+        <div className="huntingWallpaper-About" >
+            <img src = {huntingImg}/>
+        </div>
+        
+        <div> HI </div>      
+      </>
+
     )
     
   }
