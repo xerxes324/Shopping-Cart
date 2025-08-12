579d197d6ca0a255ff3447f12a8c947428b734f5
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-12 20:39:15 +0530

    logo and shop route setup

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,18 +1,19 @@
-import huntingImg from "../Assets/Hunting.jpeg";
+import { useNavigate } from "react-router-dom";
+import wallpaperImg from "../Assets/wallpaper.webp";
 
 function About() {
+
+    const nav = useNavigate();
     return (
       <>
-
-        <div className="huntingWallpaper-About" >
-            <img src = {huntingImg}/>
+        <div className="wallpaper-About" >
+            <img src = {wallpaperImg}/>
+            <button className="startShop" onClick={ () => {
+              nav("../Shop");
+            }}> Start Shopping </button>
         </div>
-        
-        <div> HI </div>      
       </>
-
     )
-    
   }
   
   export default About;
