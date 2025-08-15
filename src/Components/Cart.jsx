d9888c7a3ebe6be41d46af013139d60f688455e4
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-15 23:21:39 +0530

    Functionality and display of cart component

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,19 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 function StorageFetch(){
   const cartList = JSON.parse(localStorage.getItem("cartobject"));
-  // console.log(cartList.length, "is the length");
-  // console.log(cartList, "is the list");
-  console.log(cartList)
+  const quantity = JSON.parse(localStorage.getItem("quantity"));
+  console.log(quantity,"is the cart quantity");
+  const nav = useNavigate();
+
+  if ( cartList.length === 0){
+    return(
+      <>
+        <h2 className="emptycart"> Your cart is empty. To add items, click the button below.</h2> 
+        
+        <button className="emptycartbutton" onClick={()=>{
+          nav("../Shop")
+        }} > Store </button>
+      </>
+    )
+  }
 
   return(
 
     <>
 
       {cartList ? (
-        cartList.map((e,index) => {
+        cartList.map((e) => {
           return(
             <div className="cartImageDiv">
-
-
               <div className="cartImageDivLeft">
                 <img className="cartImages" src = {e[1]}/>
               </div>
@@ -22,25 +34,70 @@ function StorageFetch(){
               <div className="cartImageDivRight">
                 <div className="cartInfo" >
                   <p> {e[3]}</p>
-                  <p> Price : ${e[2]} </p>
+                  <p> Total Price : ${e[2] * quantity[e[4]]} </p>
                 </div>
-
-
 
                 <div className="cartButtons">
 
-                  <button  > - </button>
+                  <button className="subtract" onClick={ () =>{
+
+                    cartList.forEach((element,index) => {
+                      console.log(index,"is the index")
+                      if(element[1] === e[1] ){
+
+                        if ( element[0] === 0){
+                          cartList.splice(index,1);
+                        }
+
+                        if(element[0] > 1){
+                          element[0] -= 1;
+                          quantity[element[4]] -= 1;
+                        }
+
+                        else if(element[0] === 1) {
+                          console.log("yeyeyefgiydhfsd")
+                          quantity[element[4]] = 0;
+                          cartList.splice(index,1);
+                          
+                        }
+                        localStorage.setItem("quantity", JSON.stringify(quantity));
+                        localStorage.setItem("cartobject", JSON.stringify(cartList));
+                      }
+
+                    });
+
+                    nav("../Shop");
+                    nav("../Cart");
+                    
+                    }
+                  }
+                  > - </button>
 
                   <h3> {e[0]}</h3>
-                  <button> + </button>
 
+                  <button className="add" onClick={ () =>{
+                                        cartList.forEach((element,index) => {
+                      console.log(index,"is the index")
+                      if(element[1] === e[1] ){
 
+                        if ( element[0] <= 9){
+                          element[0] += 1;
+                          quantity[element[4]] += 1;
+                        }
+                        localStorage.setItem("quantity", JSON.stringify(quantity));
+                        localStorage.setItem("cartobject", JSON.stringify(cartList));
+                      }
+
+                    });
+
+                    nav("../Shop");
+                    nav("../Cart");
+                  }
+
+                  } 
+                  > + </button>
                 </div>
-                
               </div>
-
-
-
             </div>
           )
         })
