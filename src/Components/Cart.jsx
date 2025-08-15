17789a25708df0bd07cea15122a8681edfc7fc89
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-15 09:46:23 +0530

    Temporary commit

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,68 @@
+function StorageFetch(){
+  const cartList = JSON.parse(localStorage.getItem("cartobject"));
+  // console.log(cartList.length, "is the length");
+  // console.log(cartList, "is the list");
+  console.log(cartList)
+
+  return(
+
+    <>
+
+      {cartList ? (
+        cartList.map((e,index) => {
+          return(
+            <div className="cartImageDiv">
+
+
+              <div className="cartImageDivLeft">
+                <img className="cartImages" src = {e[1]}/>
+              </div>
+
+
+              <div className="cartImageDivRight">
+                <div className="cartInfo" >
+                  <p> {e[3]}</p>
+                  <p> Price : ${e[2]} </p>
+                </div>
+
+
+
+                <div className="cartButtons">
+
+                  <button  > - </button>
+
+                  <h3> {e[0]}</h3>
+                  <button> + </button>
+
+
+                </div>
+                
+              </div>
+
+
+
+            </div>
+          )
+        })
+      ) : (<p> Your cart is empty </p>)}
+
+    </>
+  )
+}
+
 function Cart() {
     return (
-      <div>Welcome to the CART page</div>
+      <>
+          <div className="cartBody">
+
+            <h1 className="cartHeader"> Your Items </h1>
+            <div className = "cartItems"> 
+              <StorageFetch/>
+            </div>
+          </div>
+      </>
+
     )
-    
   }
   
   export default Cart;
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -46,6 +46,31 @@ function Fetch(props){
                   props.setState(temp);
                   // props.setState(temp);
                   localStorage.setItem("quantity", JSON.stringify(temp))
+
+                  let storageTemp = [];
+                  const existingCartItems = JSON.parse(localStorage.getItem("cartobject"));
+
+                  if(storageTemp && existingCartItems){
+                    storageTemp.push(...existingCartItems);
+                  }
+                  // storageTemp.push(existingCartItems);
+                  // console.log(storageTemp);
+                  
+                  storageTemp.forEach((element, index) => {
+                    console.log(element);
+                    console.log(e[2],"is the e");
+                    if ( element[1] === e[2]){
+                      storageTemp.splice(index,1);
+                    }
+                  });
+
+                  storageTemp.push([temp[index], e[2], e[3], e[4]]);
+                  // console.log(storageTemp);
+                  // console.log(items,"is storage items)");
+                  // console.log(storageTemp)
+                  localStorage.setItem("cartobject", JSON.stringify(storageTemp));
+
+
                 }
                 
               }} > - </button>
@@ -64,7 +89,30 @@ function Fetch(props){
                   // props.setState(temp);
                   // localStorage.clear();
                   localStorage.setItem("quantity", JSON.stringify(temp));
+
+                  let storageTemp = [];
+                  const existingCartItems = JSON.parse(localStorage.getItem("cartobject"));
+
+                  if(storageTemp && existingCartItems){
+                    storageTemp.push(...existingCartItems);
+                  }
+
+                  storageTemp.forEach((element, index) => {
+                    console.log(element);
+                    console.log(e[2],"is the e");
+                    if ( element[1] === e[2]){
+                      storageTemp.splice(index,1);
+                    }
+                  });
+
+                  // storageTemp.push(existingCartItems);
                   
+
+                  storageTemp.push([temp[index], e[2], e[3], e[4]])
+                  console.log(storageTemp.length);
+                  // console.log(items,"is storage items)");
+                  // console.log(storageTemp)
+                  localStorage.setItem("cartobject", JSON.stringify(storageTemp));
                 }
               }} > + </button>
             </div>
